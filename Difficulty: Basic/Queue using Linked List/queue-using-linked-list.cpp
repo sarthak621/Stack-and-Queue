9f397654f7aac2a2215f8f2527643326f21120ab
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2025-01-11 11:56:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    
+cout << "~" << "\n";
+}
+    }
+
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{       
+        QueueNode* temp=new QueueNode(x);
+        if(front==NULL && rear==NULL){
+           temp->next=NULL;
+           front=temp;
+           rear=temp;
+        }
+        else{
+            rear->next=temp;
+            rear=temp;
+        }
+        
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code
+        
+        //cant be popped
+        if(front==NULL && rear==NULL) return -1;
+        
+        
+            QueueNode* temp=front;
+            front=front->next;
+            int data= temp->data;
+            delete temp;
+         
+            if(front==NULL){
+                rear=NULL;
+            }
+            
+            return data;
+        
+         
+        
+}
